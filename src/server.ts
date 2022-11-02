6fa9b6ca0e86935961a13cffab16898776caa2c5
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-01 22:06:37 -0300

    :rocket:

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import { PrismaClient } from '@prisma/client';
 import cors from '@fastify/cors';
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ['query'],
@@ -22,7 +24,38 @@ async function bootstrap(){
     return { count } 
   })
 
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+    const { title } = createPoolBody.parse(request.body)
 
+    const generate = new ShortUniqueId({ length: 6})
+    const code = String(generate().toUpperCase())
+    const ow = 'doawdowaowad'
+    await prisma.pool.create({
+      data: {
+        title,
+        code: code,
+      }
+    })
+
+    return { 
+      code
+    } 
+  })
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count } 
+  })
+
+  fastify.get('/guess/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count } 
+  })
 
   await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
 }
