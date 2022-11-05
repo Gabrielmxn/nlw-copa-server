5ea61eb1860e576515f6d67b6c64dfddaef45092
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-11-05 19:24:23 -0300

    :rocket: Finalização da API

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import Fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
 import cors from '@fastify/cors';
-import { z } from 'zod';
-import ShortUniqueId from 'short-unique-id';
+import jwt from '@fastify/jwt';
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+
+import { poolRoutes } from './routes/pool';
+import { authRoutes } from './routes/auth';
+import { guessRoutes } from './routes/guess';
+import { userRoutes } from './routes/user';
+import { gameRoutes } from './routes/game';
+
+
+
+
 
 
 async function bootstrap(){
@@ -18,46 +23,24 @@ async function bootstrap(){
     origin: true
   })
 
-  fastify.get('/pools/count', async () => {
-    const count = await prisma.pool.count()
-
-    return { count } 
+  // EM PRODUÇÃO PRECISA ESTÁ EM VARIÁVEL AMBIENTE
+  await fastify.register(jwt, {
+    secret: 'nlwcopa'
   })
 
-  fastify.post('/pools', async (request, reply) => {
-    const createPoolBody = z.object({
-      title: z.string(),
-    })
-    const { title } = createPoolBody.parse(request.body)
+  await fastify.register(poolRoutes);
+  await fastify.register(authRoutes);
+  await fastify.register(guessRoutes);
+  await fastify.register(userRoutes);
+  await fastify.register(gameRoutes);
 
-    const generate = new ShortUniqueId({ length: 6})
-    const code = String(generate().toUpperCase())
-    const ow = 'doawdowaowad'
-    await prisma.pool.create({
-      data: {
-        title,
-        code: code,
-      }
-    })
+ 
 
-    return { 
-      code
-    } 
-  })
+  
 
-  fastify.get('/users/count', async () => {
-    const count = await prisma.user.count()
+ 
 
-    return { count } 
-  })
-
-  fastify.get('/guess/count', async () => {
-    const count = await prisma.guess.count()
-
-    return { count } 
-  })
-
-  await fastify.listen({port: 3333, /*host: '0.0.0.0'*/})
+  await fastify.listen({port: 3333, host: '0.0.0.0'})
 }
 
 bootstrap()
